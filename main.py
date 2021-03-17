6280b678105b0f7912e9800d206c4e6116e8aeb9
nerudxlf <nerudxlf@gmail.com>
2021-03-17 13:10:53 +0600

    full project

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+import pandas as pd
+
+
+def count(data_list: list) -> tuple:
+    count_result = 0
+    for item in data_list:
+        item_split = item.split("; [")
+        count_etc = len(item_split)
+        count_russia = len(item_split)
+        for elem in item_split:
+            if elem.find("Russia") == -1:
+                count_etc -= 1
+        if count_etc != count_russia:
+            count_result += 1
+    return count_result, len(data_list)
+
+
 def main():
-    pass
+    data_df = pd.read_excel("data2020.xls")
+    address_list = data_df["Addresses"].to_list()
+    value_russia, value_etc = count(address_list)
+    print(value_russia/value_etc)
